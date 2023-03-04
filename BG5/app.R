5a94f00a551826a82bf0ad6fe0c9db8dc7331602
Camille Zahn <czahn2@uw.edu>
2023-03-04 13:52:46 -0800

    progress

--- BG5/app.R
+++ BG5/app.R
@@ -9,34 +9,31 @@ data <- read_delim("nces330_20.csv")
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Average Cost of College"),
+  titlePanel("Average Cost of College and Family Income"),
   
   
   mainPanel(
     tabsetPanel(
-      tabPanel("About", "Gotta write some stuff here."),
-      tabPanel("Plot", 
+      tabPanel("About", "A single page providing an overview of the project – what major questions are you seeking to answer, and what data will you use to answer those questions? This should be the first page that you see when you oppen the app. You should include some “additional flare” on this landing page, such as an image.",
+      img(src = "University-of-Washington-1.jpeg")),
+      tabPanel("Plot", "text about plot",
                sidebarLayout(
                  sidebarPanel(
                    checkboxGroupInput("Expense", label = "Choose expense",
                                       choices = c("Fees/Tuition", "Room/Board"),
-                                      selected = c("Fees/Tuition", "Room/Board")),
-                   radioButtons("color", "Select a color:",
-                                choices =   c("red","blue","pink"),
-                                selected = "red"),
-                   textOutput("color_text"),
+                                      selected = c("Fees/Tuition", "Room/Board"))
                  ),
                  mainPanel(plotOutput("plot"))
                )),     
       
-      tabPanel("Table",
+      tabPanel("Table", "text about table",
                sidebarLayout(
                  sidebarPanel(
                    sliderInput("year_range", label = "Choose the year range",
                                min = min(data$Year),
                                max = max(data$Year),
                                value = c(2013, 2021)),
-                   textOutput("year_text"),
+                   textOutput("year_text")
                  ),
                  mainPanel(
                    dataTableOutput("dataTable")
@@ -51,9 +48,10 @@ ui <- fluidPage(
                    textOutput("A graph or something")
                  )
                )),
-      tabPanel("Conclusion", "conclusion text"
+      tabPanel("Conclusion", "Summary takeaways, a page that hones in on at least 2 major takeaways from the project, at least one related to your analysis, and another to the dataset. Feel free to incorporate tables, graphics, or other elements to convey these conclusions."
                )
-    )))
+    ))
+)
 
 
 # server logic!
@@ -67,18 +65,13 @@ server <- function(input, output) {
   
   
   output$plot <- renderPlot({
-    color <- switch(input$color, 
-                    red = "red",
-                    blue = "blue",
-                    pink = "pink")
     data %>%
       filter(Expense %in% input$Expense) %>%
       group_by(Expense) %>%
       ggplot(aes(Year, Value, group = Expense, col = factor(Expense))) +
       ggtitle("Average Cost of Undergrad College")+
-      scale_fill_manual(values = color) +
-      geom_line(color = input$color) +
-      geom_point(color = input$color)
+      geom_line() +
+      geom_point()
   })
   output$color_text <- renderText({
     paste("You chose: ", input$color)
