e808b8c06267b705df7c8daf58912381a601183c
Camille Zahn <czahn2@uw.edu>
2023-03-08 11:16:00 -0800

    adding data

--- BG5/app.R
+++ BG5/app.R
@@ -1,9 +1,11 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(plotly)
 
 
 data <- read_delim("nces330_20.csv")
+hhIncome2013_21 <- read_delim("hhIncome2013_21.csv")
 
 # ui 
 ui <- fluidPage(
@@ -14,13 +16,14 @@ ui <- fluidPage(
   
   mainPanel(
     tabsetPanel(
-      tabPanel("About", "This website aims to relay information about the",
+      tabPanel("About", h2("Importance of Information"), "This website aims to relay information about the",
                strong("average cost of college tuition and room and board"), 
-               "across the United States. Using data, we will be answering important
+               "across the United States.", h2("Main Questions"),
+               "Using data, we will be answering important
                questions such as where, on average are the most and least expensive
                places to study? How is the cost of undergraduate education related to 
                poverty rates in specific states? And how does earning a college degree
-               impact long term financial outcomes? We will be using two main datasets
+               impact long term financial outcomes?", h2("Data"),"We will be using two main datasets
                to answer these questions. Our main data set was found from", strong("Kaggle"),
                "and the dataset is reliable and frequently updated with new information. 
                Our second dataset is from the US government, and contains data from
@@ -56,13 +59,9 @@ ui <- fluidPage(
                    dataTableOutput("dataTable")
                  )
                )),
-      tabPanel("Erik's dataset plot",
-               sidebarLayout(
-                 sidebarPanel(
-                   textOutput("Another widget here")
-                 ),
+      tabPanel("Map",
                  mainPanel(
-                   textOutput("A graph or something")
+                   plotlyOutput("plotlyMap")
                  )
                )),
       tabPanel("Additional Info", "Since our data set only provides data up to 2021, these subsequent graphics show current 2023 data for Average In-State Tuition, Average Out-Of-State Tuition, and Tuition As A Percent Of Median Income for each state",
@@ -70,7 +69,7 @@ ui <- fluidPage(
       ),
       
       
-      tabPanel("Conclusion", "Altogether, the", strong("average price of college has increased 
+      tabPanel("Conclusion", h2("Conclusion Found"), "Altogether, the", strong("average price of college has increased 
                   over the last decade"), ", as seen in the graph. Tuition and fees make up the
                majority of expenses. Despite tuition increasing, the return on investment
                in a college education also increased. It was also observed that", strong(
@@ -85,19 +84,20 @@ ui <- fluidPage(
                education. This is true for both private and public establishments. Long-term,
                these results are aimed to help simplify the financial aspect of choosing a college
                to attend. The results of our answered questions show that despite where one
-               attends college, costs are rising across the US. The data we used to answer our questions
+               attends college, costs are rising across the US.", h2("Quality of Data Used"), "The data we used to answer our questions
                are of high quality and were gathered through proper means as they accurately 
                represent the US population. The data is assumed to give unbiased results, as the 
                sample methods used are deemed to be accurate. While no specific groups are harmed
                by the data collected, some groups may be underrepresented, and the financial backgrounds
                and situations of these households are not known. This is important to note as financial
-               history and situations are often a key factor in deciding which college to attend.
-               To advance this project, it would be beneficial to study a sample of subjects, some that 
+               history and situations are often a key factor in deciding which college to attend.",
+               h2("Future Advancement of This Project"),
+               "To advance this project, it would be beneficial to study a sample of subjects, some that 
                obtained a college degree and some that did not, to observe how long-term income
                and career outcomes are related to earning a college degree.",
      img(src = "Conclusion_graph.JPG") )
     ))
-)
+
 
 
 # server logic!
@@ -129,6 +129,42 @@ server <- function(input, output) {
     paste("You chose:", input$year_range[1],"-", input$year_range[2])
   })
 }
+
+mapData <- reactive({
+  joe <- hhIncome2013_21 %>% 
+    filter(hhIncome2013_21$year %in% input$mapYear) 
+  if(nrow(joe) > 1) 
+    joe <- hhIncome2013_21 %>% 
+      filter(hhIncome2013_21$year == input$mapYear)
+  else
+    joe
+})
+
+output$plotlyMap <- renderPlotly({
+  plot_ly(mapData(), 
+          dataSet <- mapData(),
+          dataSet$hover <- with(dataSet, paste(dataSet$state,
+                                               '<br>', "Household Income", dataSet$avgHHIncome,
+                                               '<br>' , "Poverty Rate", dataSet$povertyRate)),
+          
+          l <- list(color = toRGB("white"), width = 2),
+          # specify some map projection/options
+          graphics <- list(scope = 'usa'),
+          
+          map <- plot_geo(dataSet, locationmode = 'USA-states'),
+          map <- map %>% add_trace(
+            z = dataSet$avgHHIncome, text = dataSet$hover, locations = dataSet$abbrev,
+            color = dataSet$colorsID, colors = 'Purples'
+          ),
+          
+          map <- map %>% colorbar(title = "Thousands USD"),
+          
+          map <- map %>% layout(
+            title = '2013-2021 Household Income in each U.S State<br>(Hover for breakdown)'
+            , geo = graphics )
+  )
+  map
+}) 
 
 # Run the application 
 shinyApp(ui = ui, server = server)
